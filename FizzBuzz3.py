8de3f10b2056d40053a3424b6b408a7e77630b4c
tchimphawong <83418202+tchimphawong@users.noreply.github.com>
2022-11-29 20:29:56 +0100

    Update FizzBuzz3.py

--- FizzBuzz3.py
+++ FizzBuzz3.py
@@ -1,3 +1,8 @@
+#Schreibe ein Programm das alle Zahlen von 1 bis 100 ausgibt.
+#Wenn die Zahl allerdings ein Vielfaches von 3 ist, soll statt der Zahl das Wort "Fizz" ausgegeben werden.
+#Wenn die Zahl ein Vielfaches von 5 ist, soll statt der Zahl das Wort "Buzz" ausgegeben werden.
+#Ist die Zahl sowohl ein Vielfaches von 3 als auch von 5, soll statt der Zahl das Wort "FizzBuzz" ausgegeben werden.
+
 nb = 3
 nm = 5
 for i in range(1, 101):
